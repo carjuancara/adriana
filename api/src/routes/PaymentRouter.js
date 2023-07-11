105619974e94a613b4054715a53fa8e0a147d745
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-11 16:43:04 -0300

    FEAT/UPDATE model payment

--- api/src/routes/PaymentRouter.js
+++ api/src/routes/PaymentRouter.js
@@ -4,13 +4,13 @@ const PaymentRouter = express.Router();
 const {
   postPayment,
   getPayment,
-  deletePayment,
+  changeStatePayment,
   updatePayment,
 } = require("../handlers/handlersPayment");
 
 PaymentRouter.post("/", postPayment);
-PaymentRouter.get("/:id", getPayment);
-PaymentRouter.delete("/:id", deletePayment);
-PaymentRouter.put("/:id", updatePayment);
+PaymentRouter.get("/", getPayment);
+PaymentRouter.put("/update", updatePayment);
+PaymentRouter.put("/state", changeStatePayment);
 
 module.exports = PaymentRouter;
