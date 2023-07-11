2c5c9c802093ef71d5aa1de29ab72fa72bbac111
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-11 16:23:34 -0300

    FEAT/ADD handlers roles

---
+++ api/src/handlers/handlersRoles.js
@@ -0,0 +1,13 @@
+
+const { Roles } = require('../db')
+const postSale = async (req, res) => { };
+const getSale = async (req, res) => {};
+const deleteSale = async (req, res) => {};
+const updateSale = async (req, res) => {};
+
+module.exports = {
+  postSale,
+  getSale,
+  deleteSale,
+  updateSale,
+};
