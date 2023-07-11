fc68dfd61c345d58e8b7a653f02d7a6eb5ef6fff
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-11 16:27:51 -0300

    FEAT/UPDATE model Provider

--- api/src/models/Providers.js
+++ api/src/models/Providers.js
@@ -1,55 +1,57 @@
 const { DataTypes } = require("sequelize");
-
 module.exports = (sequelize) => {
-  sequelize.define("Providers", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      unique: true,
-      autoIncrement:true
+  sequelize.define(
+    "Providers",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true,
+      },
+      company_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      direction: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      province: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      country: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      whatsapp: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      cuit: {
+        type: DataTypes.STRING(13),
+        allowNull: true,
+        validate: {
+          len: [13, 13],
+          isFormattedCuit(value) {
+            if (!/^\d{2}-\d{8}-\d{1}$/.test(value)) {
+              throw new Error("El formato del CUIT es incorrecto");
+            }
+          },
+        },
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
     },
-    company_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    
-    direction: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    city: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    province: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    country: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    whatsapp: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    cuit: {
-      type: DataTypes.STRING(13),
-      allowNull: true,
-      validate: {
-        len: 13,
-        isFormattedCuit(value) {
-          if (!/^\d{2}-\d{8}-\d{1}$/.test(value)) {
-            throw new Error('El formato del CUIT es incorrecto');
-          }
-        }
-      }
-    },
-    active: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },
-  },{
-    tableName: 'Providers' // Nombre de tabla personalizado
-  });
+    {
+      tableName: "Providers", // Nombre de tabla personalizado
+    }
+  );
 };
