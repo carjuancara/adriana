6d5e12c60c206fe879ca135568de0a20fffc5a6a
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-11 16:28:17 -0300

    FEAT/UPDATE models reason

--- api/src/models/Reason_changes.js
+++ api/src/models/Reason_changes.js
@@ -1,8 +1,7 @@
 const { DataTypes } = require("sequelize");
-
 module.exports = (sequelize) => {
   sequelize.define(
-    "Reason_changes",
+    "ReasonChanges",
     {
       id: {
         type: DataTypes.INTEGER,
@@ -13,7 +12,7 @@ module.exports = (sequelize) => {
       description: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique:true
+        unique: true,
       },
       active: {
         type: DataTypes.BOOLEAN,
@@ -21,7 +20,7 @@ module.exports = (sequelize) => {
       },
     },
     {
-      tableName: "Reason_changes", // Nombre de tabla personalizado
+      tableName: "ReasonChanges", // Nombre de tabla personalizado
     }
   );
 };
