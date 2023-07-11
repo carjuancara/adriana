c17c4377da7505d9138b846955e16f90ed0d132a
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-11 16:10:27 -0300

    FEAT/ADD roles and company create instances

--- api/index.js
+++ api/index.js
@@ -1,10 +1,28 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
 require('dotenv').config()
-const {PORT} = process.env;
+const server = require('./src/app.js');
+const { conn, Roles, Company } = require('./src/db.js');
+const { PORT } = process.env;
+
 // Syncing all the models at once.
-conn.sync({ force: true}).then(() => {
-  server.listen(PORT,"0.0.0.0" ,() => {
-    console.log('%s listening at ',PORT); // eslint-disable-line no-console
+conn.sync({ force: true }).then(async () => {
+   await Roles.bulkCreate([
+    { roles: 'admin' },
+    { roles: 'moderator' },
+    { roles: 'users' }
+  ]); 
+  await Company.create({
+    id: 1,
+    company_name: 'Adriana Indumentaria & accesorios',
+    cuit: '27-27962307-0',
+    direction: 'san martin 80',
+    city: 'Lules',
+    province: 'Tucumán',
+    country: 'Argentina',
+    whatsapp: '381462555',
+    iibb: '27-27962307-7'
+  }) 
+
+  server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server listening on port ${PORT}`);
   });
 });
