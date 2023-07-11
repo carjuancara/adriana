6619cee022917173b2627f851001d9c78e14f748
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-11 16:38:00 -0300

    FEAT/UPDATE model company

--- api/src/routes/CompanyRouter.js
+++ api/src/routes/CompanyRouter.js
@@ -2,15 +2,11 @@ const express = require('express')
 const CompanyRouter = express.Router()
 
 const {
-    postCompany,
     getCompany,
-    deleteCompany,
     updateCompany,
   } = require("../handlers/handlersCompany");
   
-  CompanyRouter.post("/", postCompany);
-  CompanyRouter.get("/:id", getCompany);
-  CompanyRouter.delete("/:id", deleteCompany);
-  CompanyRouter.put("/:id", updateCompany);
+  CompanyRouter.get("/", getCompany);
+  CompanyRouter.put("/", updateCompany);
 
 module.exports= CompanyRouter
