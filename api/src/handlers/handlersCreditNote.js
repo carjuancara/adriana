1048d7b376ad8632b9acc368567168514aaff02f
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-11 16:22:43 -0300

    FEAT/ADD handlers creditNote

---
+++ api/src/handlers/handlersCreditNote.js
@@ -0,0 +1,13 @@
+const { CreditNote, CreditDetail } = require('../db')
+const postCredit = async (req, res) => { };
+const getCredit = async (req, res) => {};
+const deleteCredit = async (req, res) => {};
+const updateCredit = async (req, res) => {};
+const enabledCredit = async (req, res)=>{}
+module.exports = {
+  postCredit,
+  getCredit,
+  deleteCredit,
+  updateCredit,
+  enabledCredit
+};
