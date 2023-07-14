3a5bedd676498e100c6d3f43a5b418116ed871d5
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-14 09:15:31 -0300

    FEAT/ modify post and create getsale

--- api/src/handlers/handlersSales.js
+++ api/src/handlers/handlersSales.js
@@ -1,42 +1,66 @@
+const { Op } = require('sequelize')
 const { Sales, SaleDetails, Articles } = require("../db");
 const postSale = async (req, res) => {
   try {
-    const { user_id, total, discount_total, payment_method_id, sales_date, productDetail } =
+    const { UserId, total, discount_total, PaymentId, sales_date, productDetail } =
       req.body;
 
     if (
-      !user_id ||
+      !UserId ||
       !total ||
       !discount_total ||
-      !payment_method_id ||
+      !PaymentId ||
       !sales_date ||
       !productDetail
     ) res.status(400).json({ Error: "missing data" });
     const newSale = await Sales.create({
-      user_id,
+      UserId,
       total,
       discount_total,
-      payment_method_id,
+      PaymentId,
       sales_date,
     });
+
+
+    //console.log('nuevaVenta.proto',newSale.__proto__)
     const newSaleDetail = await SaleDetails.bulkCreate(productDetail)
     await newSale.addSaleDetails(newSaleDetail);
-    const saleDetailsId = productDetail.map( detail => detail.article_id,)
-    console.log('saleDetailId: ',saleDetailsId)
-    //console.log('newSaleDetail: ',newSaleDetail.toJSON())
-    await  Articles.addSaleDetail(saleDetailsId)
+    const saleDetailsId = productDetail.map(detail => detail.ArticleId,)
+    const relationArticles = await Articles.findAll({
+      where: {
+        id: {
+          [Op.in]: saleDetailsId
+        }
+      }
+    })
+    const instanciasJSON = relationArticles.map(instancia => instancia.toJSON());
+    
     res.status(201).json({ msg: "the Sales changes has been addeds" });
   } catch (error) {
     res.status(400).json({ Error: error.message });
   }
 };
-const getSale = async (req, res) => { };
-const deleteSale = async (req, res) => { };
-const updateSale = async (req, res) => { };
+const getSale = async (req, res) => {
+  try {
+    const { SalesId } = req.query
+    if (!SalesId) res.status(400).json({ error: error.message })
+    const sale = await Sales.findByPk(SalesId, {
+      include: [
+        {
+          model: SaleDetails,
+          include: [Articles]
+        }
+      ]
+    });
+    res.status(200).json(sale)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+};
+
+
 
 module.exports = {
   postSale,
   getSale,
-  deleteSale,
-  updateSale,
 };
