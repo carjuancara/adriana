45727b5ddf82ab9b352282141981301d2f5ead54
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-14 09:45:02 -0300

    FEAT/ADD update compañy

--- api/src/handlers/handlersCompany.js
+++ api/src/handlers/handlersCompany.js
@@ -5,9 +5,23 @@ const getCompany = async (req, res) => {
  }
 const updateCompany = async (req, res) => {
   const { company_name, cuit, direction, city, province, country, whatsapp, iibb } = req.body
-  
-  
-  
+ 
+  const modifyCompany = await Company.Update(
+    {
+      company_name, 
+      cuit,
+      direction,
+      city,
+      province,
+      country,
+      whatsapp,
+      iibb
+    },{
+      where:{
+        id:1
+      }
+    })
+  res.status(200).json(modifyCompany)
 }
   module.exports = {
     getCompany,
