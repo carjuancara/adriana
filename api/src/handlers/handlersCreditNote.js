662d68a911b012a4d52f105767ad21f738c2d564
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-14 12:53:05 -0300

    FEAT/ADD postCredit

--- api/src/handlers/handlersCreditNote.js
+++ api/src/handlers/handlersCreditNote.js
@@ -1,9 +1,37 @@
 const { CreditNote, CreditDetail } = require('../db')
-const postCredit = async (req, res) => { };
-const getCredit = async (req, res) => {};
-const deleteCredit = async (req, res) => {};
-const updateCredit = async (req, res) => {};
-const enabledCredit = async (req, res)=>{}
+const postCredit = async (req, res) => {
+  try {
+    const { SaleId, date_original, date, UserId, direction, document, total_credit,detailsCredit } = req.body
+    if (!SaleId ||
+      !date_original ||
+      !date ||
+      !UserId ||
+      !direction ||
+      !document ||
+      !total_credit ||
+      !detailsCredit) res.status(400).json({ Error: "missing data" });
+    const newCreditNote = await CreditNote.create(
+      {
+        SaleId,
+        date_original,
+        date,
+        UserId,
+        direction,
+        document,
+        total_credit
+      })
+    const newDetailsCreditNote = await CreditDetail.bulkCreate(detailsCredit)
+
+    res.status(201).json({ msg: "the NoteCredit changes has been addeds" })
+
+  } catch (error) {
+    res.status(400).json({ Error: error.message })
+  }
+};
+const getCredit = async (req, res) => { };
+const deleteCredit = async (req, res) => { };
+const updateCredit = async (req, res) => { };
+const enabledCredit = async (req, res) => { }
 module.exports = {
   postCredit,
   getCredit,
