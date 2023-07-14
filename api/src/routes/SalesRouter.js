95fcf76570645b13dc11f84cc9ee37acd2aeace1
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-07-14 09:31:07 -0300

    FEAT/DELETE routes innecesary

--- api/src/routes/SalesRouter.js
+++ api/src/routes/SalesRouter.js
@@ -3,13 +3,10 @@ const SalesRouter = express.Router();
 const {
   postSale,
   getSale,
-  deleteSale,
-  updateSale,
 } = require("../handlers/handlersSales");
 
 SalesRouter.post("/", postSale);
 SalesRouter.get("/", getSale);
-SalesRouter.put("/update", updateSale);
-SalesRouter.put("/active", deleteSale);
+
 
 module.exports = SalesRouter;
